f615176f234125a26dad2288d2342bedba643692
TwisterrSociety <olahudia@gmail.com>
2026-02-05 09:55:59 +0700

    UPDATE

--- J0403251052_MisaelChristopherHaryanto_B1/J0403251052-B-1.R
+++ J0403251052_MisaelChristopherHaryanto_B1/J0403251052-B-1.R
@@ -8,4 +8,29 @@ mean(pengamatan)
 lenght(pengamatan)
 max(pengamatan)
 min(pengamatan)
-summary(pengamatan)
+summary(pengamatan) 
+
+getOption("defaultPackages")
+citation()
+#list default paket
+#sitasi
+demo() #list demo, keluar dari list ketikkan q
+demo(graphics) #demo program
+example(lm)
+#melihat contoh dengan nama fungsi 'lm'
+example(mean)
+
+100.1 + 234.9 + 12.01
+2+2
+sqrt(256)
+log10(100) *cos(pi) 
+cumsum(c(2,3,4,5,6)) 
+170166719 %% 31079
+48: (14*3)
+
+
+A <- matrix(c(5,4,-1,1),2,2, byrow=TRUE)
+b <- c(-10,2)
+print (A)
+print (b)
+solve (A,b)
