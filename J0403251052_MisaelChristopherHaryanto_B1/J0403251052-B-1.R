875885e4973a81f575212395244da968f646010a
TwisterrSociety <olahudia@gmail.com>
2026-02-05 02:02:52 +0000

    UPDATE

---
+++ J0403251052_MisaelChristopherHaryanto_B1/J0403251052-B-1.R
@@ -0,0 +1,11 @@
+data <-c(1, 2, 3, 4, 5)
+nama_mahasiswa <-c("Ani", "Budi", "Citra")
+x <- 17 + 45
+print(x)
+pengamatan <-c(10, 20, 30, 40)
+pengamatan * 2
+mean(pengamatan)
+lenght(pengamatan)
+max(pengamatan)
+min(pengamatan)
+summary(pengamatan)
